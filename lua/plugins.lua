cee16e2ffe333d9087a8fa8fa82d6dd9e724b970
lueythelooper <adamlsherwin@gmail.com>
2025-03-30 16:33:51 -0400

    Adding telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,4 +11,9 @@ return require('packer').startup(function(use)
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
   use 'Mofiqul/vscode.nvim'
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
