7c35cc9094e60973411027ab3080eb6da0f231df
lueythelooper <adamlsherwin@gmail.com>
2025-03-30 16:45:08 -0400

    Adding first leader keymap

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- Note, setting the mapleader in this lua does not take effect
+-- until something is mapped
+vim.g.mapleader = " "
+vim.keymap.set( "n", "<leader>pv", vim.cmd.Ex)
+
 print ("LueyTheLooper neovim config")
 require('plugins')
 
