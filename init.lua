b05289589459fc64543e3ccd5dfdaa10d46f23c7
lueythelooper <adamlsherwin@gmail.com>
2025-03-30 16:47:52 -0400

    Adding first telescope find file remap

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
 -- Note, setting the mapleader in this lua does not take effect
 -- until something is mapped
 vim.g.mapleader = " "
-vim.keymap.set( "n", "<leader>pv", vim.cmd.Ex)
 
 print ("LueyTheLooper neovim config")
 require('plugins')
 
 vim.cmd("colorscheme vscode")
+
+-- Map keys
+local builtin = require('telescope.builtin')
+vim.keymap.set( "n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
